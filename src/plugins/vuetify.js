b09e93291cc798eeabca338dcd93e83b88cb89a6
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-10-29 17:46:18 -0300

    no message

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
-import { es, en } from "vuetify/es5/locale";
-import { Ripple, Scroll } from "vuetify/lib/directives";
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import { es, en } from 'vuetify/es5/locale';
+import { Ripple, Scroll } from 'vuetify/lib/directives';
 
 Vue.use(Vuetify, {
   directives: {
@@ -13,17 +13,17 @@ Vue.use(Vuetify, {
 const options = {
   lang: {
     locales: { es, en },
-    current: "es",
+    current: 'es',
   },
   theme: {
     themes: {
       light: {
-        primary: "#568f99", //'#027087'
-        secondary: "#628390",
-        red: "#AC1315", //'#AC1315' '#BA2E38'
-        white: "#fff",
-        white_: "#F2F2F2",
-        black: "#262223",
+        primary: '#568f99', //'#027087'
+        secondary: '#628390',
+        red: '#AC1315', //'#AC1315' '#BA2E38'
+        white: '#fff',
+        white_: '#F2F2F2',
+        black: '#262223',
       },
     },
   },
--- src/views/my/settings/Users.vue
+++ src/views/my/settings/Users.vue
@@ -1,10 +1,107 @@
 <template>
-  <h1>COnfiguración de usuarios</h1>
+  <validation-observer ref="observer" v-slot="{ invalid }">
+    <form @submit.prevent="submit">
+      <validation-provider
+        v-slot="{ errors }"
+        name="Name"
+        rules="required|max:10"
+      >
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :error-messages="errors"
+          label="Name"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        name="phoneNumber"
+        :rules="{
+          required: true,
+          digits: 7,
+          regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$',
+        }"
+      >
+        <v-text-field
+          v-model="phoneNumber"
+          :counter="7"
+          :error-messages="errors"
+          label="Phone Number"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        name="email"
+        rules="required|email"
+      >
+        <v-text-field
+          v-model="email"
+          :error-messages="errors"
+          label="E-mail"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <validation-provider v-slot="{ errors }" name="select" rules="required">
+        <v-select
+          v-model="select"
+          :items="items"
+          :error-messages="errors"
+          label="Select"
+          data-vv-name="select"
+          required
+        ></v-select>
+      </validation-provider>
+      <validation-provider v-slot="{ errors }" rules="required" name="checkbox">
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Option"
+          type="checkbox"
+          required
+        ></v-checkbox>
+      </validation-provider>
+
+      <v-btn class="mr-4" type="submit" :disabled="invalid"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </form>
+  </validation-observer>
 </template>
 
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+import '../../../config/vee-validate.config';
 export default {
   name: 'Users',
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+
+  data: () => ({
+    name: '',
+    phoneNumber: '',
+    email: '',
+    select: null,
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+    checkbox: null,
+  }),
+
+  methods: {
+    submit() {
+      this.$refs.observer.validate();
+    },
+    clear() {
+      this.name = '';
+      this.phoneNumber = '';
+      this.email = '';
+      this.select = null;
+      this.checkbox = null;
+      this.$refs.observer.reset();
+    },
+  },
 };
 </script>
 
